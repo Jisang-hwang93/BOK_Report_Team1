f95d3fef80e62c280f05624779c64dca4d7f3e89
Jisang <jisanghwang@naver.com>
2020-09-07 17:24:42 +0900

    repository 정리

---
+++ Data_Crawling/naver_news/Crawling/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NaverNewsItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    media = scrapy.Field()
+    upload_date = scrapy.Field()
+    final_date = scrapy.Field()
+    content = scrapy.Field()
+    url = scrapy.Field()
